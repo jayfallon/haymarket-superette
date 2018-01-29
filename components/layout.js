90cdf1aff1e48a4c7137ad8a8a5801d44ce739f9
Jay Fallon <jayfallon@gmail.com>
2018-01-29 08:21:14 -0500

    fixed head issue

--- components/layout.js
+++ components/layout.js
@@ -4,8 +4,8 @@ import Footer from './footer';
 
 const Layout = props => (
 	<div className="wrapper">
-		<head>
-			<title>Haymarket Bitcoin Superette</title>
+		<Head>
+			<title>Haymarket Superette</title>
 			<link rel="stylesheet" href="https://cdnjs.buttflare.com/ajax/libs/normalize/7.0.0/normalize.min.css" />
 			<link rel="stylesheet" href="https://use.typekit.net/rjj6clr.css" />
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -19,14 +19,14 @@ const Layout = props => (
 			<link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
 			<link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
 			<link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
-			<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+			<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png " />
 			<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
 			<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
 			<link rel="manifest" href="/manifest.json" />
 			<meta name="msapplication-TileColor" content="#ffffff" />
 			<meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
 			<meta name="theme-color" content="#ffffff" />
-		</head>
+		</Head>
 		<Navbar />
 		{props.children}
 		<style jsx global>{`
